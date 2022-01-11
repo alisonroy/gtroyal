564343deb61b20405536c0f2c519a760a67be4ed
alisonroy <alisonroy12@gmail.com>
2022-01-11 13:08:41 +0530

    Changed

--- assets/js/process.js
+++ assets/js/process.js
@@ -1,16 +1,16 @@
 var swiper = new Swiper(".swiper-process", {
   slidesPerView: 1,
   spaceBetween: 20,
-  initialSlide: 3,
+  initialSlide: 0,
   effect: "fade",
   loop: true,
   speed: 300,
-  loop: false,
+  // loop: false,
   // delay between transitions in ms
-  autoplay: {
-    delay: 5000,
-    loop: true,
-  },
+  // autoplay: {
+  //   delay: 5000,
+  //   loop: true,
+  // },
   pagination: {
     el: ".swiper-pagination",
     clickable: false,
